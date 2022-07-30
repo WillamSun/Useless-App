2b1036bf8884a19315c813011f9beaa01488c212
Idad Wind <89760671+WillamSun@users.noreply.github.com>
2022-07-30 11:29:09 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace testforpower
+{
+    partial class Form
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title = new System.Windows.Forms.Label();
+            this.Start = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.result = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Title.Font = new System.Drawing.Font("微软雅黑 Light", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Title.Location = new System.Drawing.Point(0, 0);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(622, 45);
+            this.Title.TabIndex = 0;
+            this.Title.Text = title;
+            this.Title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Start
+            // 
+            this.Start.Font = new System.Drawing.Font("微软雅黑 Light", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Start.Location = new System.Drawing.Point(12, 70);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(602, 35);
+            this.Start.TabIndex = 0;
+            this.Start.Text = StartButtonText;
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 111);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(602, 25);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // result
+            // 
+            this.result.BackColor = System.Drawing.SystemColors.Control;
+            this.result.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.result.Font = new System.Drawing.Font("微软雅黑 Light", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.result.ForeColor = System.Drawing.Color.Gold;
+            this.result.Location = new System.Drawing.Point(0, 148);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(622, 36);
+            this.result.TabIndex = 3;
+            this.result.Text = ResultText2;
+            this.result.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "English",
+            "中文（简体）",
+            "中文（繁體）",
+            "日本語"});
+            this.comboBox1.Location = new System.Drawing.Point(501, 161);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = language;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.comboBox1_KeyPress);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 184);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Title);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form";
+            this.Text = title;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_FormClosing);
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testforpower
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        string title = "Is your computer on?";
+        string detecting = "Detecting";
+        string StartButtonText = "Start detecting";
+        string StartButtonText2 = "Detecting again";
+        string ResultText = "Your computer is on";
+        string ResultText2 = "Not detecting yet";
+        string language = "English";
+        string SuccessText = "Successed!";
+        string SuccessTitle = "Message";
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            new System.Threading.Thread(() =>
+            {
+                Start.Enabled = false;
+                comboBox1.Enabled = false;
+                Start.Text = detecting + "...";
+                result.ForeColor = Color.Blue;
+                for (int i = 0; i < 50; i++)
+                {
+                    progressBar1.Value += 2;
+                    System.Threading.Thread.Sleep(new Random().Next(0, 100));
+                }
+                System.Threading.Thread.Sleep(800);
+                progressBar1.Value = 0;
+                comboBox1.Enabled = true;
+                Start.Text = StartButtonText2;
+                result.Text = ResultText;
+                result.ForeColor = Color.FromArgb(0, 192, 0);
+                Start.Enabled = true;
+                MessageBox.Show(SuccessText + "\n" + ResultText, SuccessTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }).Start();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            new System.Threading.Thread(() =>
+            {
+                while (true)
+                {
+                    if (!Start.Enabled)
+                    {
+                        result.Text = detecting;
+                        for (int i = 0; i < 3 && !Start.Enabled; i++)
+                        {
+                            result.Text += ".";
+                            System.Threading.Thread.Sleep(80);
+                        }
+                    }
+                }
+            }).Start();
+        }
+
+        private void Form_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void comboBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                detecting = "detecting";
+                StartButtonText = "Start detecting";
+                StartButtonText2 = "Detecting again";
+                ResultText = "Your computer is on!";
+                title = "Is your computer turn on?";
+                ResultText2 = "Not detecting yet";
+                SuccessText = "Successed!";
+                SuccessTitle = "Message";
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                detecting = "检测中";
+                StartButtonText = "开始检测";
+                StartButtonText2 = "重新检测";
+                ResultText = "电脑电源已开";
+                title = "检测电脑电源是否打开";
+                ResultText2 = "未检测";
+                SuccessText = "成功！";
+                SuccessTitle = "消息";
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                detecting = "檢測中";
+                StartButtonText = "開始檢測";
+                StartButtonText2 = "重新檢測";
+                ResultText = "電腦電源已開";
+                title = "檢測電腦電源是否打開";
+                SuccessText = "成功！";
+                SuccessTitle = "消息";
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                detecting = "検査中";
+                StartButtonText = "始める検査";
+                StartButtonText2 = "再び検査";
+                ResultText = "コンピュータの電源が入っています";
+                title = "パソコンの電源は入っていますか？";
+                SuccessText = "成功！";
+                SuccessTitle = "情報";
+            }
+            language = comboBox1.Text;
+            Controls.Clear();
+            InitializeComponent();
+        }
+    }
+}
